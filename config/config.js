747b005efcd7382eaa6e0a892ca43757525ddece
Tahir-DA-Ds <tahir.adeleye@gmail.com>
2025-03-04 12:28:28 +0100

    fix

--- config/config.js
+++ config/config.js
@@ -1,12 +1,28 @@
-require('dotenv').config({ path: __dirname + "/../.env" }); 
+require('dotenv').config({ path: __dirname + '/../.env' });
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || "uqjcy2xvxurbmauw",
+    password: process.env.DB_PASS || "xVsIx8gJjpmbaF8PKi3H",
+    database: process.env.DB_NAME || "bgtligfcywwet8t88xdp",
+    host: process.env.DB_HOST || "bgtligfcywwet8t88xdp-mysql.services.clever-cloud.com",
     dialect: "mysql",
     use_env_variable: "DB_URL"
+  },
+  test: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "password",
+    database: process.env.DB_NAME || "database_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: "DB_URL",
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true, 
+      },
+    },
   }
 };
