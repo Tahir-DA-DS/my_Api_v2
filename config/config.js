a8687e874c12aca0e18b7745980b65b0603d03ac
Tahir-DA-Ds <tahir.adeleye@gmail.com>
2025-03-04 11:03:24 +0100

    database url

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    use_env_variable:process.env.DB_URL
   }
 };
